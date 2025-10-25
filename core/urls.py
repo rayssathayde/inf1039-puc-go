be3961ce863b1a8cb79ae03ab557f1c9d1d871a6
dudafonte <m.eduardafonte24@gmail.com>
2025-10-25 18:31:18 -0300

    adicionando rotas de cadastro (accounts/urls e accounts/views) e das outras telas (core/urls e core/views)

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('favorites/', views.favorites, name='favorites'),
+    path('search_result/', views.search_result, name='search_results'),
+    path('available_locations', views.available_locations, name='available_locations'),
+    
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def favorites(request):
+    return render(request, 'localizacoes_favoritas.html')
+
+def search_result(request):
+    return render(request, 'resultado_pesquisa.html')
+
+def available_locations(request):
+    return render(request, 'locais_diponiveis.html')
+
