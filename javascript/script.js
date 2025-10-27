971ab3e3ef34b8c52e7f4a736f7413580dde7bc7
RAFAELA SANTOS DE FARIA <G2511228>
2025-10-27 14:44:31 -0300

    rafaela2

--- javascript/script.js
+++ javascript/script.js
@@ -14,3 +14,70 @@
             sideMenu.classList.remove('active');
         }
     });
+
+
+
+
+
+
+
+
+
+
+
+    const meuModal = document.getElementById('meuModal');
+const btnNaoVisitar = document.getElementById('btnNaoVisitar');
+const btnSimVisitar = document.getElementById('btnSimVisitar');
+
+// Segundo modal getElement...
+const modalInstrucoes = document.getElementById('modalInstrucoes');
+const btnEntendi = document.getElementById('btnEntendi');
+
+
+
+
+// Função para abrir um modal específico(?)
+function abrirModal(modalElement) {
+    
+    fecharTodosModais(); 
+    
+    modalElement.style.display = 'flex'; 
+    document.body.classList.add('modal-aberto');
+}
+
+ 
+function fecharModal(modalElement) {
+    modalElement.style.display = 'none'; 
+}
+
+//Movimento da pagina
+function fecharTodosModais() {
+    meuModal.style.display = 'none';
+    modalInstrucoes.style.display = 'none';
+    document.body.classList.remove('modal-aberto'); 
+
+}
+
+//Mostra quando abre a pág
+window.addEventListener('load', () => {
+    abrirModal(meuModal);
+});
+
+
+// Primeiro modal:
+
+
+
+btnNaoVisitar.addEventListener('click', fecharTodosModais);
+
+
+btnSimVisitar.addEventListener('click', () => {
+    fecharModal(meuModal);
+    abrirModal(modalInstrucoes); 
+});
+
+
+
+
+//Segundo modal
+btnEntendi.addEventListener('click', fecharTodosModais);
