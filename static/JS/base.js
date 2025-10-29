5d0eb90c6a34aa9a2c047868431ec455206ecf1b
Carina <CarinallGames@gmail.com>
2025-10-29 19:10:11 -0300

    filtro implementado

--- static/JS/base.js
+++ static/JS/base.js
@@ -1,5 +1,10 @@
     const menuBtn = document.querySelector('.menu-btn');
     const sideMenu = document.getElementById('sideMenu');
+    const barra = document.getElementById("barraPesquisa"); 
+    const filtro = document.querySelector(".filtro");
+    const btnLimpar = document.querySelector(".clean"); 
+    const checkboxes = document.querySelectorAll(".acessibilidade input[type='checkbox']"); 
+console.log("JS carregado!");
 
     
     menuBtn.addEventListener('click', (e) => {
@@ -14,5 +19,21 @@
         }
     });
 
+//filtro   
 
+barra.addEventListener("focus", () => {
+  filtro.classList.add("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!barra.contains(e.target) && !filtro.contains(e.target)) {
+    filtro.classList.remove("show");
+  }
+});
+
+btnLimpar.addEventListener("click", () => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false; 
+  });
+});
 
