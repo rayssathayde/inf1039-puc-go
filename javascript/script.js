995550466e6f8b982e21f337a393b9f8c084b0ad
Carina <CarinallGames@gmail.com>
2025-10-29 13:34:33 -0300

    js do filtro

--- javascript/script.js
+++ javascript/script.js
@@ -14,3 +14,24 @@
             sideMenu.classList.remove('active');
         }
     });
+    //filtro
+const barra = document.getElementById("barraPesquisa"); 
+const filtro = document.querySelector(".filtro");
+const btnLimpar = document.querySelector(".clean"); 
+const checkboxes = document.querySelectorAll(".acessibilidade input[type='checkbox']"); 
+
+barra.addEventListener("focus", () => {
+  filtro.classList.add("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!barra.contains(e.target) && !filtro.contains(e.target)) {
+    filtro.classList.remove("show");
+  }
+});
+
+btnLimpar.addEventListener("click", () => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false; 
+  });
+});
